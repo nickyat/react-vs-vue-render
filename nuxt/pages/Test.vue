42fba0b78c4e5fea86ae20dee3e509908f98f0c2
Яцуков Николай <y@adeo.pro>
2022-05-12 01:22:22 +0300

    nuxt

---
+++ nuxt/pages/Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="r in oo()" :key="r.id">
+      <p>Row #{{r.id}}: {{r.val}}</p>
+      <span>Row #{{r.id}}: {{r.val}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+let r=[]
+for (let i=0;i<5000;i++){r.push({id:i,val:i})}
+//console.log(r)
+export default {
+  name: 'Test',
+  data(){
+    return {
+      row:r,
+    }
+  },
+  methods:{
+    oo(){  return Object.freeze(r)}
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
